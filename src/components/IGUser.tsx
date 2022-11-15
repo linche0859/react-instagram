e0e3d0fc595ccdeb63e63ab52aca381c52ee0d3e
che.lin <aa10327tw@gmail.com>
2022-11-15 14:42:16 +0800

    perf: 優化共用組件 IGUser 的渲染機制

--- src/components/IGUser.tsx
+++ src/components/IGUser.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { memo } from 'react'
 import { useDispatch } from 'react-redux'
 import { follow, unfollow } from '@/slices/followerSlice'
 
@@ -12,47 +12,49 @@ type IGUserProps = {
   size?: 'medium' | 'small'
 }
 
-const IGUser: React.FC<IGUserProps> = ({
-  id,
-  avatar,
-  account,
-  location,
-  showFollow = false,
-  isFollowing = false,
-  size = 'small'
-}) => {
-  const dispatch = useDispatch()
-  const clickFollowingHandler = () => {
-    if (id === undefined || id === null) return
-    if (isFollowing) return dispatch(unfollow(id))
-    dispatch(follow(id))
-  }
+const IGUser: React.FC<IGUserProps> = memo(
+  ({
+    id,
+    avatar,
+    account,
+    location,
+    showFollow = false,
+    isFollowing = false,
+    size = 'small'
+  }) => {
+    const dispatch = useDispatch()
+    const clickFollowingHandler = () => {
+      if (id === undefined || id === null) return
+      if (isFollowing) return dispatch(unfollow(id))
+      dispatch(follow(id))
+    }
 
-  return (
-    <div className="flex h-[70px] items-center px-4">
-      <img
-        src={avatar}
-        alt={account}
-        className={`rounded-full object-cover ${
-          size === 'small' ? 'h-10 w-10' : 'h-[60px] w-[60px]'
-        }`}
-      />
-      <div className="ml-4">
-        <p className="text-sm font-bold">{account}</p>
-        <p className="text-xs text-gray-400">{location}</p>
-      </div>
-      {showFollow && (
-        <button
-          className={`ml-auto text-xs font-bold ${
-            isFollowing ? 'text-gray-700' : 'text-blue-400'
+    return (
+      <div className="flex h-[70px] items-center px-4">
+        <img
+          src={avatar}
+          alt={account}
+          className={`rounded-full object-cover ${
+            size === 'small' ? 'h-10 w-10' : 'h-[60px] w-[60px]'
           }`}
-          onClick={clickFollowingHandler}
-        >
-          {isFollowing ? 'FOLLOWING' : 'FOLLOW'}
-        </button>
-      )}
-    </div>
-  )
-}
+        />
+        <div className="ml-4">
+          <p className="text-sm font-bold">{account}</p>
+          <p className="text-xs text-gray-400">{location}</p>
+        </div>
+        {showFollow && (
+          <button
+            className={`ml-auto text-xs font-bold ${
+              isFollowing ? 'text-gray-700' : 'text-blue-400'
+            }`}
+            onClick={clickFollowingHandler}
+          >
+            {isFollowing ? 'FOLLOWING' : 'FOLLOW'}
+          </button>
+        )}
+      </div>
+    )
+  }
+)
 
 export default IGUser
